d8ac0424bcac1d6526b30691deb4fe6c3b7e86a4
itufo <itufo@163.com>
2012-02-29 14:28:37 +0800

    port

---
+++ Source/WebCore/platform/spd/RenderThemeSpd.cpp
@@ -0,0 +1,10 @@
+#include "config.h"
+#include "RenderThemeSpd.h"
+namespace WebCore{
+
+PassRefPtr<RenderTheme> RenderTheme::themeForPage(Page* page)                                                           
+{                                                                                                                       
+     return NULL;                                                                                                    
+}
+           
+}
---
+++ Source/WebCore/platform/spd/RenderThemeSpd.h
@@ -0,0 +1,10 @@
+#ifndef RenderThemeSpd_h
+#define RenderThemeSpd_h
+#include "RenderTheme.h"
+
+namespace WebCore {
+    class RenderThemeSpd:RenderTheme{
+   
+};
+}
+#endif
