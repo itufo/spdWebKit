d968849475474b5f3264096cd37b55ea53bb0749
itufo <itufo@163.com>
2012-03-07 15:37:58 +0800

    define func

---
+++ Source/WebCore/platform/spd/FileSystemSpd.cpp
@@ -0,0 +1,33 @@
+#include "config.h"
+#include "FileSystem.h"
+
+#include "NotImplemented.h"
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <wtf/text/CString.h>
+
+namespace WebCore {
+
+CString fileSystemRepresentation(const String& path)
+{
+}
+/*
+String openTemporaryFile(const String& prefix, PlatformFileHandle& handle)
+{
+}
+
+bool unloadModule(PlatformModule module)
+{
+}
+
+String homeDirectoryPath()
+{
+}
+
+Vector<String> listDirectory(const String& path, const String& filter)
+{
+}
+*/
+}
