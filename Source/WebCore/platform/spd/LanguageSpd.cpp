bfdbc96591dc137a67aac2c8d5f68d4266a9c45c
itufo <itufo@163.com>
2012-03-14 23:02:50 +0800

    locate

--- Source/WebCore/platform/spd/LanguageSpd.cpp
+++ Source/WebCore/platform/spd/LanguageSpd.cpp
@@ -3,9 +3,10 @@
 
 #include "PlatformString.h"
 #include <wtf/Vector.h>
+#include <locale.h>
 
 namespace WebCore {
-/*
+
 static String platformLanguage()
 {
     char* localeDefault = setlocale(LC_CTYPE, 0);
@@ -15,9 +16,12 @@ static String platformLanguage()
 
     return String(localeDefault).replace('_', '-');
 }
-*/
+
 Vector<String> platformUserPreferredLanguages()
 {
+    Vector<String> userPreferredLanguages;
+    userPreferredLanguages.append(platformLanguage());
+    return userPreferredLanguages;
 }
 
 }
