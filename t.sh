6a4bfa6305d8ee23538d6b907ef39d438f3d85cd
itufo <itufo@163.com>
2012-03-30 14:12:52 +0800

    ci

--- t.sh
+++ t.sh
@@ -1,2 +1,2 @@
-g++ Source/test/test.cpp -g -Wall -O0 -lspdwebkit -levent -lcurl -lsqlite3 -ljpeg -lpng -L/usr/local/lib -I/usr/local/include 2>./TODO
+g++ Source/test/test.cpp -g -Wall -O0 -lspdwebkit -lcurl -lsqlite3 -ljpeg -lpng -L/usr/local/lib -I/usr/local/include 2>./TODO
 cat TODO|wc -l
