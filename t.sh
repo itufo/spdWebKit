970aa4a58b1b81c1431665e84419d84341c05670
itufo <itufo@163.com>
2013-05-08 16:29:48 +0800

    modify

--- t.sh
+++ t.sh
@@ -2,3 +2,8 @@ g++ -o cmd Source/test/test.cpp -g -Wall -O0 -lspdwebkit -lcurl -lsqlite3 -ljpeg
 g++ -o spdWebKit Source/test/spdWebKit.cpp -g -Wall -O0 -lspdwebkit -lcurl -lsqlite3 -ljpeg -lpng -L/usr/local/lib -I/usr/local/include 2>./TODO
 
 cat TODO|wc -l
+
+
+g++ -o spdWebKitd Source/test/spdWebKitd.cpp -g -Wall -O0 -lspider -lspdwebkit -lcurl -lsqlite3 -ljpeg -lpng -L/usr/local/lib -I/usr/local/include 2>./TODO
+
+cat TODO|wc -l
